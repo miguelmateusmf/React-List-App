9aaa396dfad62317c1b588b6df40111a502cc950
Migolo <miguelmateusmf@gmail.com>
2022-03-21 23:00:31 +0000

    first base upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { buildQueries } from '@testing-library/react';
+import Header from './components/Header';
+
+const styles = {
+  transition: 'all 1s ease-out'
+}
+
+const App = () => {
+  //const name = 'Brad'
+  //const x = true
+      //<h1>Hello from React</h1>
+      //<h2>Hello {name} {1+1}</h2>
+  
+
+  return (
+    <div className='container'>
+      <Header />
+    </div>
+
+/*<body>
+<div className='parentConfig'>
+      <div className='childConfig'>
+        <div className='childConfig2'>
+          <div className='childConfig3'>
+      </div>
+      </div>
+      </div>
+      
+      
+    </div>
+    
+    <div class="wrapper">
+  <div class="box1">One</div>
+  <div class="box2">1111111111111111111111111111111111111</div>
+  <div class="box2">2222222222222222222222222222222222222</div>
+  <div class="box2">3333333333333333333333333333333333333</div>
+  <div class="box3">three</div>
+  
+</div>
+</body>   */
+  )
+
+  }
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) => {
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='green' text='Add'/>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task tracker',
+}
+
+Header.propTypes ={
+  title: PropTypes.string.isRequired,
+}
+
+//css in js
+const headingStyle = {
+  color: 'red', backgroundColor: 'black'
+}
+export default Header
